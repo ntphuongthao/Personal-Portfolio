d0a62279f997247b6b35eeb7b25a67a6bcf46fcc
Thao Nguyen <ntphuongthao@Thaos-MacBook-Pro.local>
2022-09-29 17:51:50 -0400

    Add the navbar to the whole webpage with react-bootstrap (v.5)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Project from './components/Project/Project';
+import About from './components/About/About';
+import Resume from './components/Resume/Resume';
+import NavigationBar from './components/NavigationBar';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <div>
+        <NavigationBar />
         <nav>
-          <ul></ul>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/project">Project</Link>
+            </li>
+            <li>
+              <Link to="/resume">Resume</Link>
+            </li>
+          </ul>
         </nav>
+        <Routes>
+          <Route path="/" component={Home} />
+          <Route path="/project" component={Project} />
+          <Route path="/about" component={About} />
+          <Route path="/resume" component={Resume} />
+        </Routes>
       </div>
     </Router>
   );
 }
-
-export default App;
