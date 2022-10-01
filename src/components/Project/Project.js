7eb9a29e5ec6a77192a69c62d2e7b3a838d75b93
Thao Nguyen <ntphuongthao@Thaos-MacBook-Pro.local>
2022-10-01 02:42:33 -0400

    Custom projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,24 +2,32 @@ import ProjectCard from './ProjectCard';
 import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Project() {
+  const projects = [
+    {
+      name: 'Flight Booking',
+      link: 'https://github.com/ntphuongthao/Flight-Booking',
+      description:
+        'This project will replicate a one-way flight booker, allowing users to browse their flight with origin, destination, datetime, and the number of tickets.',
+    },
+    {
+      name: 'Facebook',
+      link: 'https://github.com/ntphuongthao/Facebook',
+      description:
+        "There are partial mockups of Facebook functions in this project. This project involved 4 main models: Users, Posts, Comments, and Likes. The following advanced features were implemented to increase the complexity and flexibility of this project - such as 'friending, friend request, login with real Facebook/Github account, feeding with all your posts and your friends' posts'",
+    },
+  ];
   return (
     <Container className="project-container">
       <Row className="project-row">
-        <Col>
-          <ProjectCard />
-        </Col>
-        <Col>
-          <ProjectCard />
-        </Col>
-      </Row>
-
-      <Row className="project-row">
-        <Col>
-          <ProjectCard />
-        </Col>
-        <Col>
-          <ProjectCard />
-        </Col>
+        {projects.map((project) => (
+          <Col>
+            <ProjectCard
+              name={project.name}
+              link={project.link}
+              description={project.description}
+            />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
--- src/components/Project/ProjectCard.js
+++ src/components/Project/ProjectCard.js
@@ -2,14 +2,16 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function ProjectCard(props) {
-  const { name, description } = props;
+  const { name, description, link } = props;
   return (
     <Card>
       <Card.Header>{name}</Card.Header>
       <Card.Body>
         <Card.Title>By Thao Nguyen</Card.Title>
         <Card.Text>{description}</Card.Text>
-        <Button variant="primary">Github</Button>
+        <form action={link}>
+          <input type="submit" value="Github" />
+        </form>
       </Card.Body>
     </Card>
   );
